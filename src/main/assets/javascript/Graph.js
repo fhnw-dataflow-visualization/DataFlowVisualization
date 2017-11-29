ce16c1f6e37a5f839f90bd13bbb866e73d6fa53d
Claudio Seitz <claudio.seitz@students.fhnw.ch>
2017-11-29 10:47:32 +0100

    Update event zoom level changed

--- src/main/assets/javascript/Graph.js
+++ src/main/assets/javascript/Graph.js
@@ -7,13 +7,22 @@ function Graph(conf, data) {
     const width = conf.width;
     const height = conf.height;
 
+    let zoom = 1;
+
     const svg = d3.select("svg")
         .attr("width", width)
         .attr("height", height);
     const viewport = svg.append('g');
     svg.call(d3.zoom()
         .scaleExtent([0.1, 2])
-        .on('zoom', () => viewport.attr('transform', d3.event.transform)));
+        .on('zoom', () => {
+            const e = d3.event.transform;
+            if (zoom !== e.k){
+                zoom = e.k;
+                console.log(`event: ${e.k}`);
+            }
+            viewport.attr('transform', e);
+        }));
 
     const tooltip = d3.select(".tooltip");
 
