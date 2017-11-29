99084d58b227ab08b32998f642826d3f22d3ba94
Claudio Seitz <claudio.seitz@students.fhnw.ch>
2017-11-29 16:08:33 +0100

    Implemented realtime level of detail changing

--- src/main/assets/javascript/Graph.js
+++ src/main/assets/javascript/Graph.js
@@ -19,19 +19,19 @@ function Graph(conf, data) {
         .scaleExtent([zoom[0], zoom[zoom.length - 1]])
         .on('zoom', () => {
             const e = d3.event.transform;
-            if (scale !== e.k) {
-                //new scale
+            viewport.attr('transform', e);
+            if (scale !== e.k) {  //new scale
                 scale = e.k;
                 if (scale < zoom[lod]) {
                     lod--;
                     console.log(`scale: ${scale}\nlevel of detail: ${lod}`);
+                    update(lod);
                 } else if (scale >= zoom[lod + 1] && lod < zoom.length - 2) {
                     lod++;
                     console.log(`scale: ${scale}\nlevel of detail: ${lod}`);
+                    update(lod);
                 }
-                // this.draw(lod);
             }
-            viewport.attr('transform', e);
         }));
 
     const tooltip = d3.select(".tooltip");
@@ -46,4 +46,9 @@ function Graph(conf, data) {
         renderer.drawNodes(lod);
         renderer.drawEdges(lod);
     };
+
+    let update = (lod) => {
+        renderer.updateNode(lod);
+        renderer.updateEdges(lod);
+    }
 }
